b37d115c2d94ece3a2d7fbd4c16328427f8ffcfc
St4bb3d <143184291+St4bb3d@users.noreply.github.com>
2024-11-22 15:57:07 -0800

    Basic Calender
    -calender added
    -current month is displayed
    -basic css
    - days as list items

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log('Happy developing ✨')
+//const variables
+const daysTag = document.querySelector(".days"),
+    currentDate = document.querySelector(".current-date"),
+    prevNextIcon = document.querySelectorAll(".icons span");
+
+// getting new date, current year and month
+let date = new Date(),
+    currYear = date.getFullYear(),
+    currMonth = date.getMonth();
+
+// storing full name of all months in array
+const months = ["January", "February", "March", "April", "May", "June", "July",
+    "August", "September", "October", "November", "December"];
+
+const renderCalendar = () =>
+{
+            let firstDayofMonth = new Date(currYear, currMonth, 1).getDay();
+            firstDayofMonth = (firstDayofMonth === 0) ? 6 : firstDayofMonth - 1; // Adjust to make Sunday the start of the week
+            let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(), // getting last date of month
+                lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+                lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+            let liTag = "";
+
+            //PREVIOUS MONTH LIST ITEMS - INACTIVE
+            for (let i = firstDayofMonth; i > 0; i--)
+            { // creating li of previous month last days
+                liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+            }
+
+            //CURRENT MONTH LIST ITEMS - ACTIVE
+            for (let i = 1; i <= lastDateofMonth; i++)
+            { // creating li of all days of current month
+                // adding active class to li if the current day, month, and year matched
+                let isToday = i === date.getDate() && currMonth === new Date().getMonth()
+                && currYear === new Date().getFullYear() ? "active" : "";
+                liTag += `<li class="${isToday}">${i}</li>`;
+            }
+
+            //NEXT MONTH LIST ITEMS - INACTIVE
+            for (let i = lastDayofMonth; i < 7; i++)
+            {
+                liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+            }
+            currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+            daysTag.innerHTML = liTag;
+}
+//calling the function
+renderCalendar();
